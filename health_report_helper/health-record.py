a7d362794349ebad05492e061a95bbc337f36820
Limin Wang <wlmnzf@126.com>
2022-04-19 10:38:38 +0800

    update

--- health_report_helper/health-record.py
+++ health_report_helper/health-record.py
@@ -1,4 +1,3 @@
-import os
 import json
 import random
 import base64
@@ -6,26 +5,19 @@ import requests
 from Crypto.Cipher import AES
 from Crypto.Util import Padding
 from bs4 import BeautifulSoup
-from random import randint
 from time import sleep
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-
-from collections import Counter
-from functools import wraps
-import threading, time
-from queue import Queue
-from urllib import parse
-import base64
-import hashlib
 import time
-import requests
+import base64
+from datetime import datetime
+import sys
+
+
 
 
 
@@ -78,22 +70,39 @@ def get_cookies(driver):
 
 def get_last_hs(driver,username_text,password_text):
     driver.get("https://user.www.gov.cn/sso/login")
-    loginname=driver.find_element_by_id("loginname")
-    password=driver.find_element_by_id("password")
-    login=driver.find_element_by_id("btn-login")
+    # loginname=driver.find_element_by_id("loginname")
+    loginname=driver.find_element(by=By.ID, value="loginname")
+    # password=driver.find_element_by_id("password")
+    password=driver.find_element(by=By.ID, value="password")
+    # login=driver.find_element_by_id("btn-login")
+    login=driver.find_element(by=By.ID, value="btn-login")
     loginname.send_keys(username_text)
     password.send_keys(password_text)
     login.click()
 
 
     sleep(3)
+    cookies=get_cookies(driver)
+    session = requests.Session()
+    session.cookies.update(cookies)
+
+
     driver.get("https://bmfw.www.gov.cn/xgbdhsktjcjgcx/index.html")
     sleep(3)
-    searchBtn=driver.find_element_by_id("searchBtn")
+    # searchBtn=driver.find_element_by_id("searchBtn")
+    searchBtn=driver.find_element(by=By.ID, value="searchBtn")
     searchBtn.click()
     sleep(3)
-    timestr=driver.find_element_by_class_name("jc-time").find_element_by_tag_name("span").text
+    # timestr=driver.find_element_by_class_name("jc-time").find_element_by_tag_name("span").text
+    timestr=driver.find_element(by=By.CLASS_NAME, value="jc-time").find_element(by=By.TAG_NAME, value="span").text
+
     # print(timestr)
+    # rd=random.random()
+    # response_valid=session.get("https://user.www.gov.cn/js/valisign?servicecode=zfwfw&time=20180621112100&sign=3ae41655a0aaa1a5758aaa8310666337&domain=www.gov.cn&rd="+str(rd))
+    # response_uid=session.get("https://user.www.gov.cn/js/islogin?servicecode=hsjycx&userinfo=false&getticket=false&rd="+str(rd))
+    # response_ticket=session.get("https://user.www.gov.cn/js/islogin?servicecode=hsjycx&userinfo=false&getticket=true&rd="+str(rd))
+
+
     return timestr
 
 def encryptAES(_p0: str, _p1: str) -> str:
@@ -121,8 +130,8 @@ def login(headers,username,password):
 
 
     options = Options()
-    options.add_argument('--headless')
-    options.add_argument('user-agent=%s'%headers["User-Agent"])
+    # options.add_argument('--headless')
+
     driver = webdriver.Chrome(options = options)
     driver.get(url_login)
 
@@ -133,8 +142,10 @@ def login(headers,username,password):
     sleep(5)
 
 
-    html = driver.find_element_by_xpath("//*").get_attribute("outerHTML")
-    imgbase64=driver.find_element_by_id("captchaImg").screenshot_as_base64
+    # html = driver.find_element_by_xpath("//*").get_attribute("outerHTML")
+    html=driver.find_element(by=By.XPATH, value="//*").get_attribute("outerHTML")
+    # imgbase64=driver.find_element_by_id("captchaImg").screenshot_as_base64
+    imgbase64=driver.find_element(by=By.ID, value="captchaImg").screenshot_as_base64
 
 
     request_url1 = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
@@ -163,13 +174,12 @@ def login(headers,username,password):
     response=session.post(url_login, data_login, headers=headers)
     return response.text,driver,session
 
-def main():
+def main(config):
     now = datetime.now();
-    print("working at ", now.month, now.day, now.hour, now.minute);
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-    username = "DG20330027"
-    password = "wlm94929"
+    username=config["username"]
+    password=config["password"]
     response=""
     driver=""
     session=""
@@ -190,15 +200,22 @@ def main():
             lastAddr = entry["CURR_LOCATION"];
             break
 
-    print("last address is:");
+    print("签到地址：");
     print(lastAddr);
+    print("\n")
 
     hssj=""
     try:
-        res=get_last_hs(driver,"15600818233","wlm94929")
+        hs_username=config["hs_username"]
+        hs_password=config["hs_password"]
+        res=get_last_hs(driver,hs_username,hs_password)
         hssj=res.split(":")[0]
     except:
         hssj="2022-04-15 15"
+
+    print("上次核酸时间");
+    print(hssj+":00");
+    print("\n")
 
     entry = entrys[0];
     
@@ -207,26 +224,35 @@ def main():
         res =session.get("https://ehallapp.nju.edu.cn/xgfw/sys/yqfxmrjkdkappnju/apply/saveApplyInfos.do?WID="+wid+"&CURR_LOCATION="+lastAddr+"&ZJHSJCSJ="+hssj+"&SFZJLN=0&IS_TWZC=1&IS_HAS_JKQK=1&JRSKMYS=1&JZRJRSKMYS=1");
         print(json.loads(res.text)["msg"]);
     else:
-        print("no work to do");
+        print("未执行操作");
 
     session.get("https://authserver.nju.edu.cn/authserver/logout");
 
-import random,time
-from datetime import datetime
-random.seed(datetime.now())
-sleeptime=random.randint(1000,1500)
-print("==========================================")
-print("启动时间：")
-print (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) 
-print("\n")
 
-print("延时：")
-print(str(sleeptime)+"s")
-# time.sleep(sleeptime)
-print("\n")
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        logging.info(sys.argv[1])
+        config.data = json.loads(sys.argv[1].replace("'", '"'))
+    
 
-print("工作时间：")
-print (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) 
-print("\n")
-main();
-print("==========================================")
+    if utils.get_GMT8_timestamp() > utils.str_to_timestamp(config.data['deadline'], '%Y-%m-%d'):
+        logging.info("超出填报日期")
+        exit(-1)
+
+    random.seed(datetime.now())
+    sleeptime=random.randint(1000,1500)
+    print("==========================================")
+    print("启动时间：")
+    print (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) 
+    print("\n")
+
+    print("延时：")
+    print(str(sleeptime)+"s")
+    # time.sleep(sleeptime)
+    print("\n")
+
+    print("工作时间：")
+    print (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) 
+    print("\n")
+    main(config.data);
+    print("==========================================")
