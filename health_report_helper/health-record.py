475b96a0af4d7fee64ea866f98e4c056b0a1666d
Limin Wang <wlmnzf@126.com>
2022-05-10 22:20:29 +0800

    update

--- health_report_helper/health-record.py
+++ health_report_helper/health-record.py
@@ -239,7 +239,9 @@ def main(config):
     "Accept-Encoding":"gzip, deflate",
     "Accept-Language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
     "User-Agent":"Mozilla/5.0 (Linux; Android 10; BMH-AN20 Build/HUAWEIBMH-AN20; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.93 Mobile Safari/537.36 cpdaily/9.0.15 wisedu/9.0.15",
-    "X-Requested-With":"com.wisedu.cpdaily.nju"
+    "X-Requested-With":"com.wisedu.cpdaily.nju",
+    "Host":"authserver.nju.edu.cn",
+    "Connection":"keep-alive",
 }
     username=config["username"]
     password=config["password"]
