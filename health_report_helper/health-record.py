5bd405b5548c77afe89bb7a2c20c0fd91957f058
Limin Wang <wlmnzf@126.com>
2022-05-10 23:09:20 +0800

    update

--- health_report_helper/health-record.py
+++ health_report_helper/health-record.py
@@ -1,4 +1,3 @@
-from email import header
 import json
 import random
 import base64
@@ -21,7 +20,6 @@ import sys
 import re
 import sys
 import logging
-from io import BytesIO
 
 import config
 import utils
@@ -29,7 +27,7 @@ import utils
 import urllib3
 
 urllib3.disable_warnings()
-session=""
+
 
 
 
@@ -80,18 +78,8 @@ def get_cookies(driver):
         jar.set(i['name'], i['value'],domain=i['domain'],path=i['path'])  
     return jar
 
-def get_last_hs(username_text,password_text):
-    options = Options()
-    options.add_argument('--headless')
-
-    driver = webdriver.Chrome(options = options)
-    # driver.set_page_load_timeout(20)
-
-    try:
-        driver.get("https://user.www.gov.cn/sso/login")
-    except Exception:
-        driver.execute_script('window.stop()')
-
+def get_last_hs(driver,username_text,password_text):
+    driver.get("https://user.www.gov.cn/sso/login")
     # loginname=driver.find_element_by_id("loginname")
     loginname=driver.find_element(by=By.ID, value="loginname")
     # password=driver.find_element_by_id("password")
@@ -102,17 +90,14 @@ def get_last_hs(username_text,password_text):
     password.send_keys(password_text)
     login.click()
 
-    # logging.info("axx")
+
     sleep(3)
-    # cookies=get_cookies(driver)
-    # session = requests.Session()
-    # session.cookies.update(cookies)
+    cookies=get_cookies(driver)
+    session = requests.Session()
+    session.cookies.update(cookies)
 
-    try:
-        driver.get("https://bmfw.www.gov.cn/xgbdhsktjcjgcx/index.html")
-    except Exception:
-        driver.execute_script('window.stop()')
 
+    driver.get("https://bmfw.www.gov.cn/xgbdhsktjcjgcx/index.html")
     sleep(3)
     # searchBtn=driver.find_element_by_id("searchBtn")
     searchBtn=driver.find_element(by=By.ID, value="searchBtn")
@@ -120,6 +105,14 @@ def get_last_hs(username_text,password_text):
     sleep(3)
     # timestr=driver.find_element_by_class_name("jc-time").find_element_by_tag_name("span").text
     timestr=driver.find_element(by=By.CLASS_NAME, value="jc-time").find_element(by=By.TAG_NAME, value="span").text
+
+    # print(timestr)
+    # rd=random.random()
+    # response_valid=session.get("https://user.www.gov.cn/js/valisign?servicecode=zfwfw&time=20180621112100&sign=3ae41655a0aaa1a5758aaa8310666337&domain=www.gov.cn&rd="+str(rd))
+    # response_uid=session.get("https://user.www.gov.cn/js/islogin?servicecode=hsjycx&userinfo=false&getticket=false&rd="+str(rd))
+    # response_ticket=session.get("https://user.www.gov.cn/js/islogin?servicecode=hsjycx&userinfo=false&getticket=true&rd="+str(rd))
+
+
     return timestr
 
 def encryptAES(_p0: str, _p1: str) -> str:
@@ -141,136 +134,95 @@ def decrypt(m: str, key: str) -> str:
         cipher = AES.new(key.strip().encode('utf-8'), AES.MODE_CBC, iv);
         return _unpad(cipher.decrypt(m[AES.block_size:])).decode('utf-8');
 
-def needCaptcha(username):
-    global session
-    url = 'https://authserver.nju.edu.cn/authserver/needCaptcha.html?username={}'.format(
-            username)
-    r = session.post(url)
-    if 'true' in r.text:
-        return True
-    else:
-        return False
-
-def getCaptchaCode():
-    global session
-    url = 'https://authserver.nju.edu.cn/authserver/captcha.html'
-    res = session.get(url, stream=True)
-    # img=BytesIO(res.content)
-    # byte_data = byte_data.getvalue()# 从字节流管道中获取二进制
-    base64_str = base64.b64encode(res.content).decode("ascii")# 二进制转base64
-    return base64_str
-
-
 def login(headers,username,password,config):
     url_login = r'https://authserver.nju.edu.cn/authserver/login?service=http://ehallapp.nju.edu.cn/xgfw/sys/yqfxmrjkdkappnju/apply/getApplyInfoList.do'
-    global session
-    session = requests.Session()
-    session.headers.update({
-            'User-Agent': "Mozilla/5.0 (Linux; Android 11; M2006J10C Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36  cpdaily/8.2.7 wisedu/8.2.7})"
-        })
 
-    r = session.get(url_login)
-    lt = re.search(
-            r'<input type="hidden" name="lt" value="(.*)"/>', r.text).group(1)
-    execution = re.search(
-            r'<input type="hidden" name="execution" value="(.*)"/>', r.text).group(1)
-    eventId = re.search(
-            r'<input type="hidden" name="_eventId" value="(.*)"/>', r.text).group(1)
-    rmShown = re.search(
-            r'<input type="hidden" name="rmShown" value="(.*)"', r.text).group(1)
-    pwdDefaultEncryptSalt = re.search(
-            r'var pwdDefaultEncryptSalt = "(.*)"', r.text).group(1)
+    options = Options()
+    options.add_argument('--headless')
 
+    driver = webdriver.Chrome(options = options)
+    driver.get(url_login)
+
+    element = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.ID, "qr_img"))
+        )
+
+    sleep(5)
+
+
+    # html = driver.find_element_by_xpath("//*").get_attribute("outerHTML")
+    # html=driver.find_element(by=By.XPATH, value="//*").get_attribute("outerHTML")
+    # imgbase64=driver.find_element_by_id("captchaImg").screenshot_as_base64
+    imgbase64=driver.find_element(by=By.ID, value="captchaImg").screenshot_as_base64
+
+
+    request_url1 = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
+    request_url2 = "https://aip.baidubce.com/rest/2.0/ocr/v1/handwriting"
     
-    verycode = ""
-    if needCaptcha(username):
-        imgbase64 = getCaptchaCode()
-        request_url1 = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
-        request_url2 = "https://aip.baidubce.com/rest/2.0/ocr/v1/handwriting"
-        verycode=get_verycode(request_url1,imgbase64,config)
-        if len(verycode)!=4:
-            return ""
+    verycode=get_verycode(request_url1,imgbase64,config)
+ 
+    if len(verycode)!=4:
+        driver.close()
+        return driver,"",""
+
+    loginname=driver.find_element(by=By.ID, value="username")
+    loginpassword=driver.find_element(by=By.ID, value="password")
+    captchaResponse=driver.find_element(by=By.ID, value="captchaResponse")
+    login=driver.find_element(by=By.XPATH, value="//p[@id='cpatchaDiv']/following-sibling::p[1]").find_element(by=By.TAG_NAME,value="button")
     
-    data = {
-            'username': username,
-            'password': encryptAES(password, pwdDefaultEncryptSalt),
-            'lt': lt,
-            'dllt': 'userNamePasswordLogin',
-            'execution': execution,
-            '_eventId': eventId,
-            'rmShown': rmShown,
-            'captchaResponse': verycode
-        }
-    session.headers.update(headers)
-    r = session.post(url_login, data=data)
-    return r.text
-    
+    loginname.send_keys(username)
+    loginpassword.send_keys(password)
+    captchaResponse.send_keys(verycode)
+    login.click()
+
+    # soup = BeautifulSoup(html, 'html.parser')
+    # soup.select_one("#pwdDefaultEncryptSalt").attrs['value']
+    # data_login = {
+    #     'username': username, 
+    #     'password': encryptAES(password, soup.select_one("#pwdDefaultEncryptSalt").attrs['value']),
+    #     'captchaResponse':verycode,
+    #     'lt' : soup.select_one('[name="lt"]').attrs['value'], 
+    #     'dllt' : "userNamePasswordLogin",
+    #     'execution' : soup.select_one('[name="execution"]').attrs['value'], 
+    #     '_eventId' : soup.select_one('[name="_eventId"]').attrs['value'], 
+    #     'rmShown' : soup.select_one('[name="rmShown"]').attrs['value'], 
+    # }
+
+    # response=""
+    # try:
+    #     response=session.post(url_login, data_login, headers=headers)
+    # except:
+    #     return "",driver,session
+
+    sleep(5)
+    jsontext=driver.find_element(by=By.XPATH,value="//*").text
+    cookies=get_cookies(driver)
+    return driver,cookies,jsontext
 
 def main(config):
     
-    # headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-    headers = {
-    "Host":"authserver.nju.edu.cn",
-    "Connection":"keep-alive",
-    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "User-Agent":"Mozilla/5.0 (Linux; Android 10; BMH-AN20 Build/HUAWEIBMH-AN20; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.93 Mobile Safari/537.36 cpdaily/9.0.15 wisedu/9.0.15;",
-    "Referer":"http://ehallapp.nju.edu.cn/xgfw/sys/mrjkdkappnju/index.html",
-    "Accept-Encoding":"gzip, deflate, br",
-    "Accept-Language":"zh-CN,zh;q=0.9",
-    "X-Requested-With":"com.wisedu.cpdaily.nju",
-    "Cache-Control":"max-age=0",
-    "Content-Type":"application/x-www-form-urlencoded",
-    "Origin":"https://authserver.nju.edu.cn",
-    "Referer":"https://authserver.nju.edu.cn/authserver/login?service=http://ehallapp.nju.edu.cn/xgfw/sys/yqfxmrjkdkappnju/apply/getApplyInfoList.do",
-    "sec-ch-ua":'" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
-    "sec-ch-ua-mobile":"?0",
-    "sec-ch-ua-platform":'"Linux"',
-    "Sec-Fetch-Dest":"document",
-    "Sec-Fetch-Mode":"navigate",
-    "Sec-Fetch-Site":"same-origin",
-    "Sec-Fetch-User":"?1",
-    "Upgrade-Insecure-Requests":"1",
-    "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36"
-}
-
-    headers = {
-    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Accept-Encoding":"gzip, deflate",
-    "Accept-Language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-    "User-Agent":"Mozilla/5.0 (Linux; Android 10; BMH-AN20 Build/HUAWEIBMH-AN20; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.93 Mobile Safari/537.36 cpdaily/9.0.15 wisedu/9.0.15",
-    "Host":"authserver.nju.edu.cn",
-    "Connection":"keep-alive",
-}
-
-    headers = {
-    "Host":"authserver.nju.edu.cn",
-    "Connection":"keep-alive",
-    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "User-Agent":"Mozilla/5.0 (Linux; Android 10; BMH-AN20 Build/HUAWEIBMH-AN20; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.93 Mobile Safari/537.36 cpdaily/9.0.15 wisedu/9.0.15;",
-    "Accept-Encoding":"gzip, deflate, br",
-    "Accept-Language":"zh-CN,zh;q=0.9",
-    "X-Requested-With":"com.wisedu.cpdaily.nju",
-    "Content-Type":"application/x-www-form-urlencoded",
-    "Origin":"https://authserver.nju.edu.cn",
-    "Referer":"https://authserver.nju.edu.cn/authserver/login?service=http://ehallapp.nju.edu.cn/xgfw/sys/yqfxmrjkdkappnju/apply/getApplyInfoList.do",
-    "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36"
-}
     username=config["username"]
     password=config["password"]
-
+    cookies=""
+    driver=""
     jsontext=""
 
     try:
-        jsontext=login(headers,username,password,config)
+        driver,cookies,jsontext=login(headers,username,password,config)
 
         tries=5
         while "CURR_LOCATION" not in jsontext  and tries>=0:
+            driver.quit()
             tries-=1
             logging.info("登录失败：正在进行第"+str(5-tries)+"次尝试\n")
             logging.info(jsontext)
-            jsontext=login(headers,username,password,config)
+            driver,cookies,jsontext=login(headers,username,password,config)
             
+
+
+
         if "CURR_LOCATION" not in jsontext:
             raise Exception('登录失败!')
             logging.info("\n登录失败!")
@@ -299,10 +251,9 @@ def main(config):
     try:
         hs_username=config["hs_username"]
         hs_password=config["hs_password"]
-        res=get_last_hs(hs_username,hs_password)
+        res=get_last_hs(driver,hs_username,hs_password)
         hssj=res.split(":")[0]
-    except Exception as e:
-        logging.info(e)
+    except:
         logging.info("获取最新核酸时间失败，本次采用历史核酸时间！");
         
 
@@ -312,8 +263,8 @@ def main(config):
 
     entry = entrys[0];
     
-
-    global session
+    session = requests.Session()
+    session.cookies.update(cookies)
 
     HEADERS = {
     "Host":"ehallapp.nju.edu.cn",
@@ -348,11 +299,15 @@ def main(config):
     else:
         logging.info("未执行操作");
 
+    # session.get("https://authserver.nju.edu.cn/authserver/logout");
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         config.data = json.loads(sys.argv[1].replace("'", '"'))
+
+    # logging.info(config.data)
+    
 
     if utils.get_GMT8_timestamp() > utils.str_to_timestamp(config.data['deadline'], '%Y-%m-%d'):
         logging.info("超出填报日期")
